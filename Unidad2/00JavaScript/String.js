5c6b3a1a070a5427c781da1c0419af87237d4d29
Mishell <mishell.castillo@unl.edu.ec>
2026-06-12 13:07:18 -0500

    Feat: Learning Javascript for noobscis

---
+++ Unidad2/00JavaScript/String.js
@@ -0,0 +1,15 @@
+let string1 = 'Hola, mundo'
+let string2 = "JavaScript"
+let string3 = `${string1} feliz :)`
+let string4 = string1 + ' ' + string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+
+let frase = 'JavaScript es genial'
+console.log(frase.length)
+console.log(frase.toUpperCase())
+console.log(frase.toLowerCase())
+console.log(frase.substring(0, 10))
---
+++ Unidad2/00JavaScript/tipos_datos.js
@@ -0,0 +1,35 @@
+// PRIMITIVOS
+let nombre = 'Maria'
+
+// NUMERICO
+let edad = 19
+
+//  BOOLEANOS
+let esMayorDeEdad = true
+
+// null
+let noHayValor = null
+
+// undefined
+let noDefinido = undefined
+
+// symbol valor unico
+let simboloUnico = Sumbol('único')
+
+ // datos gigantes bigint
+ let numeroGrande  = 2n
+ 
+
+ // COMPLETOS
+ // object
+ let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+//arrays
+let frutas = ['uva', 'piña', 'pera', 'manzana', 'kiwi']
+
+// FUNCIONES
+function saludar(){}
+
