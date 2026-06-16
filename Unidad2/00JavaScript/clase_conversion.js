a000f4e6aeec146db04fe359826a7c2779545bfe
Mishell <mishell.castillo@unl.edu.ec>
2026-06-16 11:35:11 -0500

    Feat: Clase del 16-06-2026, Mishell Castillo

---
+++ Unidad2/00JavaScript/clase_conversion.js
@@ -0,0 +1,14 @@
+const string = '47'
+const integer = parseInt(string)
+console.log(typeof integer)
+
+const stringDecimal = '47.5'
+const float = parseInt(stringDecimal)
+console.log(stringDecimal)
+
+const binary = '10010'
+const decimal = parseInt(binary, 2)
+console.log(decimal)
+
+const suma = 5 - true
+console.log(suma)
---
+++ Unidad2/00JavaScript/clase_numbers.js
@@ -0,0 +1,35 @@
+// Tipos de datos numericos
+// entero y decimal
+
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+// Notación científica
+const cientifico = 5e3
+
+// Representación de otros contextos
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+// Operaciones Aritméticas
+// Sumas, restas, multiplicaciones y divisiones
+const suma = 3 + 4
+const resta = 4 - 4
+multiplicacion = 4 * 7
+division = 10 / 2
+
+//Modulos de exponenciacion
+const modulo = 15 % 8
+
+//Resultado de presicion
+const exponenciacion = 0.1 + 0.2
+console.log(exponenciacion.toFixed(1))
+
+const raizCuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+console.log(raizCuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
+
